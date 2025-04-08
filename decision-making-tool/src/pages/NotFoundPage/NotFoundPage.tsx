f87be86f036569426018e7c64ff20e94b6c899b1
Anton <lipan48@gmail.com>
2025-04-08 22:59:15 +0300

    feat: implement 404 page

--- decision-making-tool/src/pages/NotFoundPage/NotFoundPage.tsx
+++ decision-making-tool/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,10 +1,12 @@
 import { ReactNode } from "react";
 import { Link } from "react-router-dom";
+import styles from './NotFoundPage.module.scss'
 
 function NotFoundPage(): ReactNode {
   return (
     <main>
       <h1>OOoops</h1>
+      <img src="/404.svg" alt="Error, 404" className={ styles['error-img'] } />
       <p>There is no such page. Are you sure that URL is right?</p>
       <Link  to='/'>Go Home</Link>
     </main>
